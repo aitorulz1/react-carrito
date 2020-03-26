8e04c2050e351e2114fbd1ab03bfc4f10174dddd
Aitor Arina <aitorulz1@gmail.com>
2020-03-26 19:13:16 +0100

    muy avanzado con los datos en consts

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -1,13 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Pregunta from './components/Pregunta'
+import Formulario from './components/Formulario'
 
 
 function App() {
 
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ resto, guardarResto ] = useState(0);
+  const [ gastos, guardarGastos ] = useState([]);
+  const [ gasto, guardarGasto ] = useState({})
+
 
   return (
     <Fragment>
-      <div>
+      <div className="container">
+        <header>
+          <h1>Gasto Semanal</h1>
 
+            <div className="contenido-principal contenido">
+
+              <Pregunta 
+                guardarPresupuesto={guardarPresupuesto}
+                guardarResto={guardarResto}
+              />
+                
+                <div className="row">
+
+                  <div className="one-half column">
+                    <Formulario 
+                      guardarGasto = {guardarGasto}
+                    />
+                  </div>
+
+                  <div className="one-half column">
+                    2
+                  </div>
+
+                </div>
+
+            </div>
+        </header>
       </div>
     </Fragment>
   );
